01d50a4c748f103d46c8d52ac5f75eeba0fc2553
skotnyjdvor <smtmsflwlss@gmail.com>
2026-07-15 12:25:27 +0200

    Add new D3Teams brand logo

--- src/main.jsx
+++ src/main.jsx
@@ -170,7 +170,7 @@ const nav = [
 function Logo() {
   return (
     <div className="logo">
-      <div className="logoMark">D3</div>
+      <img className="logoImage" src="/d3teams-logo.png" alt="D3Teams" />
       <div>
         <strong>D3TEAMS</strong>
         <small>RACE OPERATIONS</small>
